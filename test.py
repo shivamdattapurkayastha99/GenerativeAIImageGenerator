c2e13a7df31f8b761a33d61a3e0c00bdb394e144
Shivam Datta  Purkayastha <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2024-07-13 23:39:45 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+
+import openai
+from config import key
+from openai import OpenAI
+openai.api_key = key
+
+
+client = OpenAI()
+
+response = client.images.generate(
+  model="dall-e-3",
+  prompt="a white siamese cat",
+  size="1024x1024",
+  quality="standard",
+  n=1,
+)
+
+image_url = response.data[0].url
